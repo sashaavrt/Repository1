0bdd6451264e798ceef97a3f6c5ff48e8fedc7e4
sasha <suede.pouch@gmail.com>
2022-09-17 16:33:44 +0300

    hh

---
+++ решение_кв_уравнений/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите a");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b");
+double  b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите c");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double D;
+D = (b * b) - (4 * a * c);
+double D1 = Math.Sqrt(D);
+
+if (D < 0)
+
+{
+    Console.WriteLine("Нет корней!");
+}
+
+else
+
+{
+    double x1 = (b * -1 + D1) / (2 * a);
+    double x2 = (b * -1 - D1) / (2 * a);
+    string a1 = Convert.ToString(x1);
+    string a2 = Convert.ToString(x2);
+    Console.WriteLine(a1);
+    Console.WriteLine(a2);
+}
+
